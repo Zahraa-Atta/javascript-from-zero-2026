68c34a69b029e3c0d4960f4b07b4b3ac2da11204
Zahraa Atta <zahraaatta5@gmail.com>
2026-03-05 03:16:46 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,24 @@
+// Values and Variables
+console.log('zahraa');
+console.log(24);
+
+let firstName = "zahraa";
+
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+// Variable name conventions
+let zahraaSara = 'ZS';
+let $function = 27;
+
+let person = 'zahraa';
+let PI = 3.14;
+
+let myFirstJob = 'Coder';
+let myCurrentJob = 'Teacher';
+
+let job1 = 'programmer';
+let job2 = 'teacher';
+
+console.log(myFirstJob)
